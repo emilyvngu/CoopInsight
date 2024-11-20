5deeac2bc1187e52ef8f0069b35c486411bb42c8
Nanofaux <ilmc57@hotmail.com>
2024-11-19 23:19:47 -0500

    Update DDL and add example queries

--- database-files/CoopInsight.sql
+++ database-files/CoopInsight.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS CoopInsight;
 USE CoopInsight;
 CREATE TABLE IF NOT EXISTS `User`
 (
-    UserID      INT PRIMARY KEY,
+    UserID      INT PRIMARY KEY AUTO_INCREMENT,
     FirstName   VARCHAR(50) NOT NULL,
     LastName    VARCHAR(50) NOT NULL,
     Email       VARCHAR(50) NOT NULL,
@@ -13,69 +13,70 @@ CREATE TABLE IF NOT EXISTS `User`
 );
 CREATE TABLE IF NOT EXISTS Skill
 (
-    SkillID   INT PRIMARY KEY,
+    SkillID   INT PRIMARY KEY AUTO_INCREMENT,
     SkillName VARCHAR(50) NOT NULL UNIQUE
 );
 CREATE TABLE IF NOT EXISTS Industry
 (
-    IndustryID   INT PRIMARY KEY,
+    IndustryID   INT PRIMARY KEY AUTO_INCREMENT,
     IndustryName VARCHAR(50) NOT NULL UNIQUE
 );
 CREATE TABLE IF NOT EXISTS StudentMajor
 (
-    Major      VARCHAR(50) NOT NULL PRIMARY KEY,
-    IndustryID INT REFERENCES Industry (IndustryID)
+    Major      VARCHAR(50) PRIMARY KEY,
+    IndustryID INT NOT NULL REFERENCES Industry (IndustryID) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 CREATE TABLE IF NOT EXISTS Company
 (
-    CompanyID          INT PRIMARY KEY,
+    CompanyID          INT PRIMARY KEY AUTO_INCREMENT,
     CompanyName        VARCHAR(50)  NOT NULL,
-    IndustryID         INT REFERENCES Industry (IndustryID),
+    IndustryID         INT          NOT NULL REFERENCES Industry (IndustryID) ON UPDATE CASCADE ON DELETE RESTRICT,
     CompanyDescription VARCHAR(500) NOT NULL,
-    AdminID            INT REFERENCES `User` (UserID)
+    AdminID            INT          NOT NULL REFERENCES `User` (UserID) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 CREATE TABLE IF NOT EXISTS JobListing
 (
-    JobID          INT PRIMARY KEY,
+    JobID          INT PRIMARY KEY AUTO_INCREMENT,
     Name           VARCHAR(50)  NOT NULL,
-    CompanyID      INT REFERENCES Company (CompanyID),
-    Major          VARCHAR(50) REFERENCES StudentMajor (Major),
+    CompanyID      INT          NOT NULL REFERENCES Company (CompanyID) ON UPDATE CASCADE ON DELETE CASCADE,
+    Major          VARCHAR(50)  NOT NULL REFERENCES StudentMajor (Major) ON UPDATE CASCADE ON DELETE CASCADE,
     MinGPA         FLOAT(4)     NOT NULL,
-    IndustryID     INT REFERENCES Industry (IndustryID),
+    IndustryID     INT          NOT NULL REFERENCES Industry (IndustryID) ON UPDATE CASCADE ON DELETE RESTRICT,
     Posted         DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     JobDescription VARCHAR(500) NOT NULL,
-    SkillID        INT REFERENCES Skill (SkillID)
+    SkillID        INT          NOT NULL REFERENCES Skill (SkillID) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 CREATE TABLE IF NOT EXISTS Employee
 (
-    EmployeeID INT PRIMARY KEY,
-    UserID     INT REFERENCES `User` (UserID),
+    EmployeeID INT PRIMARY KEY AUTO_INCREMENT,
+    UserID     INT         NOT NULL REFERENCES `User` (UserID) ON UPDATE CASCADE ON DELETE CASCADE,
     HiredOn    DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     Role       VARCHAR(50) NOT NULL DEFAULT 'Employee',
-    JobID      INT REFERENCES JobListing (JobID),
-    CompanyID  INT REFERENCES Company (CompanyID)
+    JobID      INT         NOT NULL REFERENCES JobListing (JobID) ON UPDATE CASCADE ON DELETE RESTRICT,
+    CompanyID  INT         NOT NULL REFERENCES Company (CompanyID) ON UPDATE CASCADE ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS Student
 (
-    StudentID    INT PRIMARY KEY,
-    UserID       INT REFERENCES `User` (UserID),
+    StudentID    INT PRIMARY KEY AUTO_INCREMENT,
+    UserID       INT         NOT NULL REFERENCES `User` (UserID) ON UPDATE CASCADE ON DELETE CASCADE,
     GPA          FLOAT(4)    NOT NULL,
-    Skill        INT REFERENCES Skill (SkillID),
-    Major        VARCHAR(50) NOT NULL,
-    SupervisorID INT REFERENCES Employee (EmployeeID)
+    Skill        INT         NOT NULL REFERENCES Skill (SkillID) ON UPDATE CASCADE ON DELETE RESTRICT,
+    Major        VARCHAR(50) NOT NULL REFERENCES StudentMajor (Major) ON UPDATE CASCADE ON DELETE RESTRICT,
+    SupervisorID INT REFERENCES Employee (EmployeeID) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 CREATE TABLE IF NOT EXISTS Applicant
 (
-    ApplicantID INT PRIMARY KEY,
-    StudentID   INT REFERENCES Student (StudentID),
-    JobID       INT REFERENCES JobListing (JobID),
+    ApplicantID INT PRIMARY KEY AUTO_INCREMENT,
+    StudentID   INT         NOT NULL REFERENCES Student (StudentID) ON UPDATE CASCADE ON DELETE CASCADE,
+    JobID       INT         NOT NULL REFERENCES JobListing (JobID) ON UPDATE CASCADE ON DELETE CASCADE,
     Status      VARCHAR(50) NOT NULL DEFAULT 'Pending'
 );
 CREATE TABLE IF NOT EXISTS JobHistory
 (
-    JobHistoryID        INT PRIMARY KEY,
-    UserID              INT REFERENCES `User` (UserID),
-    JobID               INT REFERENCES JobListing (JobID),
+    JobHistoryID        INT PRIMARY KEY AUTO_INCREMENT,
+    UserID              INT                                NOT NULL REFERENCES `User` (UserID) ON UPDATE CASCADE ON DELETE CASCADE,
+    JobID               INT                                NOT NULL REFERENCES JobListing (JobID) ON UPDATE CASCADE ON DELETE CASCADE,
+    CompanyID           INT                                NOT NULL REFERENCES Company (CompanyID) ON UPDATE CASCADE ON DELETE CASCADE,
     Wage                INT                                NOT NULL,
     EmploymentStartDate DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
     EmploymentEndDate   DATETIME,
@@ -83,21 +84,22 @@ CREATE TABLE IF NOT EXISTS JobHistory
 );
 CREATE TABLE IF NOT EXISTS Rating
 (
-    RatingID                    INT PRIMARY KEY,
+    RatingID                    INT PRIMARY KEY AUTO_INCREMENT,
     OverallRating               INT          NOT NULL,
-    REVIEW                      VARCHAR(500) NOT NULL,
+    Review                      VARCHAR(500) NOT NULL,
     WorkCultureRating           INT          NOT NULL,
     CompensationRating          INT          NOT NULL,
     WorkLifeBalanceRating       INT          NOT NULL,
     LearningOpportunitiesRating INT          NOT NULL,
-    JobID                       INT REFERENCES JobListing (JobID),
-    StudentID                   INT REFERENCES Student (StudentID)
+    JobID                       INT REFERENCES JobListing (JobID) ON UPDATE CASCADE ON DELETE CASCADE,
+    CompanyID                   INT REFERENCES Company (CompanyID) ON UPDATE CASCADE ON DELETE CASCADE,
+    UserID                      INT          NOT NULL REFERENCES User (UserID) ON UPDATE CASCADE ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS Offer
 (
-    OfferID     INT PRIMARY KEY,
-    ApplicantID INT REFERENCES Student (StudentID),
-    JobID       INT REFERENCES JobListing (JobID),
+    OfferID     INT PRIMARY KEY AUTO_INCREMENT,
+    ApplicantID INT         NOT NULL REFERENCES Student (StudentID) ON UPDATE CASCADE ON DELETE CASCADE,
+    JobID       INT         NOT NULL REFERENCES JobListing (JobID) ON UPDATE CASCADE ON DELETE CASCADE,
     Wage        INT         NOT NULL,
     StartDate   DATETIME    NOT NULL,
     EndDate     DATETIME,
@@ -105,14 +107,14 @@ CREATE TABLE IF NOT EXISTS Offer
 );
 CREATE TABLE IF NOT EXISTS ErrorLog
 (
-    LogID            INT PRIMARY KEY,
-    UserID           INT REFERENCES `User` (UserID),
+    LogID            INT PRIMARY KEY AUTO_INCREMENT,
+    UserID           INT          NOT NULL REFERENCES `User` (UserID) ON UPDATE CASCADE ON DELETE CASCADE,
     ErrorDescription VARCHAR(500) NOT NULL,
     ErrorDate        DATETIME     NOT NULL
 );
 CREATE TABLE IF NOT EXISTS JobSkill
 (
-    JobSkillID INT PRIMARY KEY,
-    JobID      INT REFERENCES JobListing (JobID),
-    SkillID    INT REFERENCES Skill (SkillID)
+    JobSkillID INT PRIMARY KEY AUTO_INCREMENT,
+    JobID      INT NOT NULL REFERENCES JobListing (JobID) ON UPDATE CASCADE ON DELETE CASCADE,
+    SkillID    INT NOT NULL REFERENCES Skill (SkillID) ON UPDATE CASCADE ON DELETE CASCADE
 );
