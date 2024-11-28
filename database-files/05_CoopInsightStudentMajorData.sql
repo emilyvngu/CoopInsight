46f9fba95283b8603f19accb6dadaef23d1cb59d
Andrew Pluzhnikov <andrew@Andrews-MacBook-Air.local>
2024-11-28 09:26:31 -0800

    Added mocked StudentMajor data

---
+++ database-files/05_CoopInsightStudentMajorData.sql
@@ -0,0 +1,12 @@
+USE CoopInsight;
+
+insert into StudentMajor (Major, IndustryID) values ('Training', 23);
+insert into StudentMajor (Major, IndustryID) values ('Support', 23);
+insert into StudentMajor (Major, IndustryID) values ('Engineering', 15);
+insert into StudentMajor (Major, IndustryID) values ('Business Development', 22);
+insert into StudentMajor (Major, IndustryID) values ('Accounting', 16);
+insert into StudentMajor (Major, IndustryID) values ('Sales', 5);
+insert into StudentMajor (Major, IndustryID) values ('Research and Development', 3);
+insert into StudentMajor (Major, IndustryID) values ('Human Resources', 3);
+insert into StudentMajor (Major, IndustryID) values ('Legal', 23);
+insert into StudentMajor (Major, IndustryID) values ('Services', 14);
