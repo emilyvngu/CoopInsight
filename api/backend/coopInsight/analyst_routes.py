164ddacd83fff753928035ee7ae3a3a36b364811
emilyngu8 <nguyen.emily@northeastern.edu>
2024-12-05 10:02:29 -0500

    remove commenting

--- api/backend/coopInsight/analyst_routes.py
+++ api/backend/coopInsight/analyst_routes.py
@@ -39,20 +39,6 @@ def get_job_ratings():
     the_response.status_code = 200
     return the_response
 
-"""
-(
-    RatingID                    INT PRIMARY KEY AUTO_INCREMENT,
-    OverallRating               INT          NOT NULL,
-    Review                      VARCHAR(500) NOT NULL,
-    WorkCultureRating           INT          NOT NULL,
-    CompensationRating          INT          NOT NULL,
-    WorkLifeBalanceRating       INT          NOT NULL,
-    LearningOpportunitiesRating INT          NOT NULL,
-    JobID                       INT REFERENCES JobListing (JobID) ON UPDATE CASCADE ON DELETE CASCADE,
-    CompanyID                   INT REFERENCES Company (CompanyID) ON UPDATE CASCADE ON DELETE CASCADE,
-    UserID                      INT          NOT NULL REFERENCES User (UserID) ON UPDATE CASCADE ON DELETE CASCADE
-);"""
-
 
 #------------------------------------------------------------
 # Get all jobs and companies
@@ -78,26 +64,3 @@ def get_companies_and_jobs():
         the_response = make_response(jsonify(theData))
         the_response.status_code = 200
         return the_response
-
-
-"""
-CREATE TABLE IF NOT EXISTS Company
-(
-    CompanyID          INT PRIMARY KEY AUTO_INCREMENT,
-    CompanyName        VARCHAR(50)  NOT NULL,
-    IndustryID         INT          NOT NULL REFERENCES Industry (IndustryID) ON UPDATE CASCADE ON DELETE RESTRICT,
-    CompanyDescription VARCHAR(500) NOT NULL,
-    AdminID            INT          NOT NULL REFERENCES `User` (UserID) ON UPDATE CASCADE ON DELETE RESTRICT
-);
-CREATE TABLE IF NOT EXISTS JobListing
-(
-    JobID          INT PRIMARY KEY AUTO_INCREMENT,
-    Name           VARCHAR(50)  NOT NULL,
-    CompanyID      INT          NOT NULL REFERENCES Company (CompanyID) ON UPDATE CASCADE ON DELETE CASCADE,
-    Major          VARCHAR(50)  NOT NULL REFERENCES StudentMajor (Major) ON UPDATE CASCADE ON DELETE CASCADE,
-    MinGPA         FLOAT(4)     NOT NULL,
-    IndustryID     INT          NOT NULL REFERENCES Industry (IndustryID) ON UPDATE CASCADE ON DELETE RESTRICT,
-    Posted         DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    JobDescription VARCHAR(500) NOT NULL,
-    SkillID        INT          NOT NULL REFERENCES Skill (SkillID) ON UPDATE CASCADE ON DELETE RESTRICT
-"""
