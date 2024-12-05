27a926c635de2c628d4f8b9000bb4397905495dc
emilyngu8 <nguyen.emily@northeastern.edu>
2024-12-05 09:44:56 -0500

    Added basics for page 72

---
+++ app/src/pages/72_Industry_Analysis.py
@@ -0,0 +1,19 @@
+import logging
+logger = logging.getLogger(__name__)
+import streamlit as st
+from modules.nav import SideBarLinks
+import requests
+import pandas as pd
+
+# Configure logger
+logger = logging.getLogger(__name__)
+
+# Set Streamlit page configuration
+st.set_page_config(layout="wide", page_title="Job Ratings Dashboard")
+
+# Display the appropriate sidebar links for the role of the logged-in user
+SideBarLinks()
+
+# Base URL for the Flask backend
+BASE_URL = "http://localhost:8501" 
+
