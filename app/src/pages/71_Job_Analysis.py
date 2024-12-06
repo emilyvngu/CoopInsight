a95cb59144a9293aefffbce834861d01db27b896
emilyngu8 <nguyen.emily@northeastern.edu>
2024-12-06 04:10:23 -0500

    test

--- app/src/pages/71_Job_Analysis.py
+++ app/src/pages/71_Job_Analysis.py
@@ -44,6 +44,8 @@ def fetch_job_ratings(company_name, job_name):
                 (df['CompanyName'].str.strip().str.lower() == company_name.strip().lower()) &
                 (df['JobName'].str.strip().str.lower() == job_name.strip().lower())]
 
+        print(filtered_df)
+
         if not filtered_df.empty:
             aggregated_reviews = " ".join(filtered_df['Review'].tolist())
             average_rating = filtered_df['OverallRating'].mean()
