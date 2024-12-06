090c894354d2e4c411a9c12b99ed125d6c084e5d
emilyngu8 <nguyen.emily@northeastern.edu>
2024-12-06 07:34:26 -0500

    updated admin

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -77,9 +77,6 @@ def CompanyAnalysisNav():
 #### ------------------------ System Admin Role ------------------------
 def AdminPageNav():
     st.sidebar.page_link("pages/20_Admin_Home.py", label="System Admin", icon="👤")
-    st.sidebar.page_link(
-        "pages/21_ML_Model_Mgmt.py", label="ML Model Management", icon="🏢"
-    )
 
 
 # --------------------------------Links Function -----------------------------------------------
