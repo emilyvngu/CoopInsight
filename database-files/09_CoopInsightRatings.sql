a1b4ac07ba31c16ce0ead70ece6c950fa34d2303
Andrew Cincotta <cincotta.a@northeastern.edu>
2024-12-06 17:15:44 -0500

    fixed 09

--- database-files/09_CoopInsightRatings.sql
+++ database-files/09_CoopInsightRatings.sql
@@ -1,47 +1,18 @@
 USE CoopInsight;
 
-insert into Rating (OverallRating, Review, WorkCultureRating, CompensationRating, WorkLifeBalanceRating,
+INSERT INTO Rating (OverallRating, Review, WorkCultureRating, CompensationRating, WorkLifeBalanceRating, 
                     LearningOpportunitiesRating, JobID, CompanyID, UserID)
-values (3, 'Great Company, but I think there are better.', 3, 3, 3, 3, 1, 1, 1);
-insert into Rating (OverallRating, Review, WorkCultureRating, CompensationRating, WorkLifeBalanceRating,
-                    LearningOpportunitiesRating, JobID, CompanyID, UserID)
-values (4, 'I love working here. The people are great and the work is challenging.', 5, 4, 5, 4, 2, 2, 2);
-insert into Rating (OverallRating, Review, WorkCultureRating, CompensationRating, WorkLifeBalanceRating,
-                    LearningOpportunitiesRating, JobID, CompanyID, UserID)
-values (5, 'I have been working here for 5 years and I love it. The work is challenging and the people are great.', 5,
-        5, 5, 5, 3, 3, 3);
-insert into Rating (OverallRating, Review, WorkCultureRating, CompensationRating, WorkLifeBalanceRating,
-                    LearningOpportunitiesRating, JobID, CompanyID, UserID)
-values (2, 'I do not like working here. The work is boring and the people are not friendly.', 2, 2, 1, 2, 4, 4, 4);
-insert into Rating (OverallRating, Review, WorkCultureRating, CompensationRating, WorkLifeBalanceRating,
-                    LearningOpportunitiesRating, JobID, CompanyID, UserID)
-values (3, 'I like working here. The work is challenging and the people are friendly.', 3, 3, 4, 3, 5, 5, 5);
-insert into Rating (OverallRating, Review, WorkCultureRating, CompensationRating, WorkLifeBalanceRating,
-                    LearningOpportunitiesRating, JobID, CompanyID, UserID)
-values (5, 'Awesome Company. Would recommend it to anyone.', 5, 4, 5, 5, 2, 2, 2);
-insert into Rating (OverallRating, Review, WorkCultureRating, CompensationRating, WorkLifeBalanceRating,
-                    LearningOpportunitiesRating, JobID, CompanyID, UserID)
-values (4, 'Great Company. The work is challenging and the people are great.', 4, 3, 4, 4, 3, 3, 3);
-insert into Rating (OverallRating, Review, WorkCultureRating, CompensationRating, WorkLifeBalanceRating,
-                    LearningOpportunitiesRating, JobID, CompanyID, UserID)
-values (3, 'The company could use some better management, but overall it''s decent.', 1, 3, 3, 3, 4, 4, 4);
-insert into Rating (OverallRating, Review, WorkCultureRating, CompensationRating, WorkLifeBalanceRating,
-                    LearningOpportunitiesRating, JobID, CompanyID, UserID)
-values (1, 'I strongly dislike the company''s management here, as they are not very good at their jobs.', 1, 1, 1, 1, 5,
-        5, 5);
-insert into Rating (OverallRating, Review, WorkCultureRating, CompensationRating, WorkLifeBalanceRating,
-                    LearningOpportunitiesRating, JobID, CompanyID, UserID)
-values (2, 'The company is okay, but the work is boring and the people are not very friendly.', 2, 2, 2, 2, 2, 2, 2);
-insert into Rating (OverallRating, Review, WorkCultureRating, CompensationRating, WorkLifeBalanceRating,
-                    LearningOpportunitiesRating, JobID, CompanyID, UserID)
-values (3,
-        'The company is decent, however I think there are better options when it comes to the work force in this field. The CEO is funny, though :)',
-        5, 3, 3, 3, 3, 3, 3);
-insert into Rating (OverallRating, Review, WorkCultureRating, CompensationRating, WorkLifeBalanceRating,
-                    LearningOpportunitiesRating, JobID, CompanyID, UserID)
-values (1, 'I wish the company would listen. I tried talking to HR about my concerns and they did nothing.', 1, 1, 1, 1,
-        4, 4, 4);
-insert into Rating (OverallRating, Review, WorkCultureRating, CompensationRating, WorkLifeBalanceRating,
-                    LearningOpportunitiesRating, JobID, CompanyID, UserID)
-values (1, 'I wish the company would listen. I tried talking to HR about my concerns and they did nothing.', 1, 1, 1, 1,
-        16, 1, 4);
+VALUES
+    (3, 'Great Company, but I think there are better.', 3, 3, 3, 3, 1, 1, 1),
+    (4, 'I love working here. The people are great and the work is challenging.', 5, 4, 5, 4, 2, 2, 2),
+    (5, 'I have been working here for 5 years and I love it. The work is challenging and the people are great.', 5, 5, 5, 5, 3, 3, 3),
+    (2, 'I do not like working here. The work is boring and the people are not friendly.', 2, 2, 1, 2, 4, 4, 4),
+    (3, 'I like working here. The work is challenging and the people are friendly.', 3, 3, 4, 3, 5, 5, 5),
+    (5, 'Awesome Company. Would recommend it to anyone.', 5, 4, 5, 5, 2, 2, 2),
+    (4, 'Great Company. The work is challenging and the people are great.', 4, 3, 4, 4, 3, 3, 3),
+    (3, 'The company could use some better management, but overall it''s decent.', 1, 3, 3, 3, 4, 4, 4),
+    (1, 'I strongly dislike the company''s management here, as they are not very good at their jobs.', 1, 1, 1, 1, 5, 5, 5),
+    (2, 'The company is okay, but the work is boring and the people are not very friendly.', 2, 2, 2, 2, 2, 2, 2),
+    (3, 'The company is decent, however I think there are better options when it comes to the work force in this field. The CEO is funny, though :)', 5, 3, 3, 3, 3, 3, 3),
+    (1, 'I wish the company would listen. I tried talking to HR about my concerns and they did nothing.', 1, 1, 1, 1, 4, 4, 4),
+    (1, 'I wish the company would listen. I tried talking to HR about my concerns and they did nothing.', 1, 1, 1, 1, 16, 1, 4);
