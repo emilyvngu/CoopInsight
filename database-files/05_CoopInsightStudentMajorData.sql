421bb52d69b8331953aa42c40aa6beba576928e7
Andrew Cincotta <cincotta.a@northeastern.edu>
2024-12-06 17:06:09 -0500

    fixed 05

--- database-files/05_CoopInsightStudentMajorData.sql
+++ database-files/05_CoopInsightStudentMajorData.sql
@@ -1,12 +1,14 @@
 USE CoopInsight;
 
-insert into StudentMajor (Major, IndustryID) values ('Training', 23);
-insert into StudentMajor (Major, IndustryID) values ('Support', 23);
-insert into StudentMajor (Major, IndustryID) values ('Engineering', 15);
-insert into StudentMajor (Major, IndustryID) values ('Business Development', 22);
-insert into StudentMajor (Major, IndustryID) values ('Accounting', 16);
-insert into StudentMajor (Major, IndustryID) values ('Sales', 5);
-insert into StudentMajor (Major, IndustryID) values ('Research and Development', 3);
-insert into StudentMajor (Major, IndustryID) values ('Human Resources', 3);
-insert into StudentMajor (Major, IndustryID) values ('Legal', 23);
-insert into StudentMajor (Major, IndustryID) values ('Services', 14);
+INSERT INTO StudentMajor (Major, IndustryID)
+VALUES 
+    ('Training', 23),
+    ('Support', 23),
+    ('Engineering', 15);
+    ('Business Development', 22),
+    ('Accounting', 16),
+    ('Sales', 5),
+    ('Research and Development', 3),
+    ('Human Resources', 3),
+    ('Legal', 23),
+    ('Services', 14);
